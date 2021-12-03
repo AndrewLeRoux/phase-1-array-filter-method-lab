219671542357cbbe2bb782ebf58dc30b8eee393e
Andrew LeRoux <andrewleroux2639@gmail.com>
2021-12-03 14:26:20 -0800

    commit tree e9711f59cc5a443e46329f8e17e307a50a0c889e

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+// findMatching
+
+function findMatching (driversNames, string) {
+    return driversNames.filter(function (obj) {
+        return obj.toLowerCase() === string.toLowerCase()
+    })
+}
+// fuzzyMatch
+function fuzzyMatch (driversNames, letters) {
+    return driversNames.filter(function (obj) {
+        return obj.substring(0, letters.length) === letters
+    })
+}
+
+// matchName
+function matchName (driversObjs, string) {
+    return driversObjs.filter(function (obj) {
+        return obj.name === string
+    })
+}
